c6e20a1245863a74d4b7b99f45153b1a90fd88c6
noura <noura.tu19@gmail.com>
2024-09-15 13:41:31 +0300

    solve lab 2

---
+++ main.js
@@ -0,0 +1,34 @@
+let container = document.getElementById('container');
+let url = "https://rickandmortyapi.com/api/character";
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        let characters = data.results;
+        characters.forEach(character => {
+            let card = document.createElement('div');
+            let cardBody = document.createElement('div');
+            let image = document.createElement('img');
+            let title = document.createElement('h5');
+            let text = document.createElement('p');
+
+         
+            card.className = 'card col-md-4 col-lg-3  col-sm-6 col-xs-12 m-3'; 
+            cardBody.className = 'card-body';
+            title.className = 'card-header';
+             
+
+            
+            title.textContent = character.name;
+            image.src = character.image;
+            
+
+         
+            card.appendChild(image);
+            card.appendChild(cardBody);
+            cardBody.appendChild(title);
+           
+            container.appendChild(card);
+        });
+    })
+    
